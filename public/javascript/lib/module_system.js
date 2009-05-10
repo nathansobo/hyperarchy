a79ec9b0de0d12811cfc0ee74dafd811a34cfd63
Nathan Sobo <nathansobo@nathan-sobos-macbook-pro.local>
2009-05-10 03:37:25 -0700

    Added basics of constructor definition.

--- public/javascript/lib/module_system.js
+++ public/javascript/lib/module_system.js
@@ -1,7 +1,18 @@
 ModuleSystem = {
-  module: function(qualified_module_name, attributes) {
-    var module = this.create_module_path(qualified_module_name);
-    this.mixin(module, attributes);
+  constructor: function(qualified_constructor_name, prototype_properties) {
+    var constructor_name = qualified_constructor_name.split(".").pop();
+    var containing_module = this.create_module_containing_constructor(qualified_constructor_name);
+
+    constructor = function() {
+    }
+    this.mixin(constructor.prototype, prototype_properties);
+    return containing_module[constructor_name] = constructor;
+  },
+
+  module: function(qualified_module_name, properties) {
+    var module = this.create_module_path(qualified_module_name.split("."));
+    this.mixin(module, properties);
+    return module;
   },
 
   mixin: function(target, module) {
@@ -12,19 +23,13 @@ ModuleSystem = {
     return target;
   },
 
-  resolve_qualified_module_name: function(qualified_module_name) {
-    var path = qualified_module_name.split(".");
-    var current_module = window;
-    for (var i = 0; i < path.length; i++) {
-      var path_fragment = path[i];
-      if (!current_module[path_fragment]) return false;
-      current_module = current_module[path_fragment];
-    }
-    return current_module;
+  create_module_containing_constructor: function(qualified_constructor_name) {
+    var qualified_constructor_path = qualified_constructor_name.split(".");
+    var containing_module_path = qualified_constructor_path.slice(0, qualified_constructor_path.length - 1);
+    return this.create_module_path(containing_module_path);
   },
 
-  create_module_path: function(qualified_module_name) {
-    var path = qualified_module_name.split(".");
+  create_module_path: function(path) {
     var current_module = window;
     for (var i = 0; i < path.length; ++i) {
       var path_fragment = path[i];
--- spec/javascript/lib/module_system_spec.js
+++ spec/javascript/lib/module_system_spec.js
@@ -2,13 +2,75 @@
 
 Screw.Unit(function(c) { with(c) {
   describe("ModuleSystem", function() {
-    describe("#module", function() {
-      context("when given a top-level module name and a properties hash", function() {
-        context("when no module with that name is defined", function() {
-          after(function() {
-            delete window['Foo'];
+    describe(".constructor", function() {
+      describe("naming and prototype property definition", function() {
+        after(function() {
+          delete window['Foo'];
+        });
+
+        context("when given a top-level name and a properties hash", function() {
+          it("creates a constructor with that name, defining the given properties on its prototype", function() {
+            expect(window['Foo']).to(be_undefined);
+
+            ModuleSystem.constructor("Foo", {
+              foo: "foo",
+              bar: "bar"
+            });
+
+            expect(Foo).to_not(be_undefined);
+            expect(Foo.prototype.foo).to(equal, "foo");
+            expect(Foo.prototype.bar).to(equal, "bar");
+          });
+        });
+
+        context("when given a qualified name and a properties hash", function() {
+          context("when no modules along the given path exist", function() {
+            it("creates all modules along the path and creates the constructor at its terminus whose prototype has the given properties", function() {
+              expect(window['Foo']).to(be_undefined);
+
+              ModuleSystem.constructor("Foo.Bar.Baz", {
+                foo: "foo",
+                bar: "bar"
+              });
+
+              expect(Foo).to_not(be_undefined);
+              expect(Foo.Bar).to_not(be_undefined);
+              expect(Foo.Bar.Baz).to_not(be_undefined);
+              expect(Foo.Bar.Baz.prototype.foo).to(equal, "foo");
+              expect(Foo.Bar.Baz.prototype.bar).to(equal, "bar");
+            });
           });
 
+          context("when modules along the given path exists, but not the terminus", function() {
+            before(function() {
+              ModuleSystem.module("Foo", {
+                foo: "foo"
+              });
+            });
+
+            it("creates any module that does not yet exist, but leaves existing modules intact", function() {
+              ModuleSystem.constructor("Foo.Bar.Baz", {
+                foo: "foo",
+                bar: "bar"
+              });
+
+              expect(Foo.foo).to_not(be_undefined);
+              expect(Foo.Bar.Baz).to_not(be_undefined);
+              expect(Foo.Bar.Baz.prototype.foo).to(equal, "foo");
+              expect(Foo.Bar.Baz.prototype.bar).to(equal, "bar");
+            });
+          });
+        });
+      });
+    });
+
+    describe(".module", function() {
+      after(function() {
+        delete window['Foo'];
+      });
+
+      context("when given a top-level name and a properties hash", function() {
+        context("when no module with that name is defined", function() {
           it("defines a new top level module by the given name with the properties", function() {
             expect(window['Foo']).to(be_undefined);
             ModuleSystem.module("Foo", {
@@ -30,10 +92,6 @@ Screw.Unit(function(c) { with(c) {
             });
           });
 
-          after(function() {
-            delete window['Foo'];
-          });
-
           it("mixes the given properties into the existing module", function() {
             ModuleSystem.module("Foo", {
               bar: "bar2",
@@ -47,10 +105,6 @@ Screw.Unit(function(c) { with(c) {
       });
 
       context("when given a qualified module name and a properties hash", function() {
-        after(function() {
-          delete window['Foo'];
-        });
-
         context("when no modules along the given path exist", function() {
           it("creates all modules along the path and installs the properties at its terminus", function() {
             expect(window['Foo']).to(be_undefined);
@@ -69,6 +123,43 @@ Screw.Unit(function(c) { with(c) {
         });
 
         context("when modules along the given path exists, but not the terminus", function() {
+          before(function() {
+            ModuleSystem.module("Foo", {
+              foo: "foo"
+            });
+          });
+
+          it("creates any module that does not yet exist, but leaves existing modules intact", function() {
+            ModuleSystem.module("Foo.Bar.Baz", {
+              foo: "foo",
+              bar: "bar"
+            });
+            
+            expect(Foo.foo).to_not(be_undefined);
+            expect(Foo.Bar.Baz).to_not(be_undefined);
+            expect(Foo.Bar.Baz.foo).to(equal, "foo");
+            expect(Foo.Bar.Baz.bar).to(equal, "bar");
+          });
+        });
+
+        context("when all modules, including the terminus, exist", function() {
+          before(function() {
+            ModuleSystem.module("Foo.Bar.Baz", {
+              foo: "foo",
+              bar: "bar"
+            });
+          });
+
+          it("mixes the given properties into the existing modules", function() {
+            ModuleSystem.module("Foo.Bar.Baz", {
+              bar: "bar2",
+              baz: "baz"
+            });
+
+            expect(Foo.Bar.Baz.foo).to(equal, "foo");
+            expect(Foo.Bar.Baz.bar).to(equal, "bar2");
+            expect(Foo.Bar.Baz.baz).to(equal, "baz");
+          });
         });
       });
     });
