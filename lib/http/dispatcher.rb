b80995e8b9a5510fed00d2fb2a197dc6847c0524
Nathan Sobo <nathansobo@nathan-sobos-macbook-pro.local>
2009-05-31 21:54:17 -0700

    Dispatcher assigns session_id on all resources. Added dispatcher_spec.

--- lib/http/dispatcher.rb
+++ lib/http/dispatcher.rb
@@ -18,10 +18,13 @@ module Http
       locate_resource(request.path_info).send(request.request_method.downcase, request.params)
     end
 
-    def locate_resource(path)
-      res = path_parts(path).inject(root) do |resource, child_resource_name|
+    def locate_resource(path, session_id)
+      root.session_id = session_id
+      path_parts(path).inject(root) do |resource, child_resource_name|
         if resource
-          resource.locate(child_resource_name)
+          next_resource = resource.locate(child_resource_name)
+          next_resource.session_id = session_id
+          next_resource
         else
           nil
         end
---
+++ spec/lib/http/dispatcher_spec.rb
@@ -0,0 +1,30 @@
+require File.expand_path("#{File.dirname(__FILE__)}/../../hyperarchy_spec_helper")
+
+module Http
+  describe Dispatcher do
+    attr_reader :dispatcher
+
+    before do
+      @dispatcher = Dispatcher.instance
+    end
+
+    describe "#locate_resource" do
+      it "starting with #root, successively calls #locate on resources with each fragment of the given path, assigning #current_session_id on each" do
+        mock_root = Object.new
+        resource_1 = Object.new
+        resource_2 = Object.new
+
+        session_id = 'sample-session_id'
+
+        stub(dispatcher).root { mock_root }
+        mock(mock_root).session_id=(session_id)
+        mock(mock_root).locate('resource_1') { resource_1 }
+        mock(resource_1).session_id=(session_id)
+        mock(resource_1).locate('resource_2') { resource_2 }
+        mock(resource_2).session_id=(session_id)
+
+        dispatcher.locate_resource("/resource_1/resource_2", session_id).should == resource_2
+      end
+    end
+  end
+end
