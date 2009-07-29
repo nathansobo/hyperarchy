a6018d4d607e7ab663be88856d8a79c4e110c286
Nathan Sobo <nathansobo@nathan-sobos-macbook-pro.local>
2009-07-28 22:23:38 -0700

    Added a script that can camelize all the js. But I didn't like how the source looked, so I decided not to apply it at this time.

---
+++ script/camelize_js.rb
@@ -0,0 +1,25 @@
+require 'find' 
+
+Find.find(ARGV[0]) do |file_name| 
+  Find.prune if file_name =~ /vendor$/
+  if File.file?(file_name) && file_name =~ /\.(js|css)/
+    lines = File.open(file_name) { |file| file.readlines }
+    
+    changed = false
+    lines.each do |line|
+      next if line =~ %r{^//=}
+      if line.gsub!(/(_)(.)/) { $2.upcase }
+        changed = true 
+        puts line
+      end
+    end
+
+    next unless changed      
+    File.open(file_name, "w") do |file|
+      p file_name
+      lines.each do |line|
+        file.write(line)
+      end
+    end    
+  end
+end
