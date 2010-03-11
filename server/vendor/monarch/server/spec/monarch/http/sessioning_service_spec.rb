ebafb593e7d22741cf76c86e1e6479f2bcf4d6c4
Nathan Sobo <nathansobo@gmail.com>
2010-03-11 10:30:33 -0600

    Fixed db:load_fixtures. When no session corresponding to the current session_id cookie is present in the database, a new session is created.

--- server/vendor/monarch/server/spec/monarch/http/sessioning_service_spec.rb
+++ server/vendor/monarch/server/spec/monarch/http/sessioning_service_spec.rb
@@ -31,13 +31,31 @@ module Http
       end
 
       context "when a '_session_id' cookie is present in the request" do
-        it "sets #session_id in the forwarded request withoun creating a new Session, and does not add a 'Set-Cookie' header to the response" do
-          incoming_request.cookies['_session_id'] = "sample-session-id"
+        context "when a session corresponding to this id actually exists in the database" do
+          it "sets #session_id in the forwarded request without creating a new Session, and does not add a 'Set-Cookie' header to the response" do
+            incoming_request.cookies['_session_id'] = Session.create.session_id
 
-          dont_allow(Session).create
-          response = Response.new(*sessioning_service.call(incoming_request.env))
-          forwarded_request.session_id.should == incoming_request.cookies['_session_id']
-          response.cookies['_session_id'].should be_nil
+            dont_allow(Session).create
+            response = Response.new(*sessioning_service.call(incoming_request.env))
+            forwarded_request.session_id.should == incoming_request.cookies['_session_id']
+            response.cookies['_session_id'].should be_nil
+          end
+        end
+
+        context "when no session corresponding to this id actually exists in the database" do
+          it "creates a new Session, sets #session_id in the forwarded request, and adds a 'Set-Cookie' header to the response" do
+            incoming_request.cookies['_session_id'] = "bogus-session-id"
+
+            session_id = nil
+            mock.proxy(Session).create do |session|
+              session_id = session.session_id
+              session
+            end
+
+            response = Response.new(*sessioning_service.call(incoming_request.env))
+            forwarded_request.session_id.should == session_id
+            response.cookies['_session_id'].should == session_id
+          end
         end
       end
     end
