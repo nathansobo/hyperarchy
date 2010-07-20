5494108c66567b698fd92dd6145c5bd1e3dd06d2
Nathan Sobo <nathansobo@gmail.com>
2010-07-20 13:14:46 -0300

    Fixed bug where comet update operations did not camelize the keys

--- server/vendor/monarch/client/lib/monarch/http/command_batch.js
+++ server/vendor/monarch/client/lib/monarch/http/command_batch.js
@@ -10,7 +10,6 @@ _.constructor("Monarch.Http.CommandBatch", {
     this.future = new Monarch.Http.AjaxFuture();
 
     if (this.commands.length > 0) {
-      console.debug("mutating: ", this.wireRepresentation());
       this.server.post(Repository.originUrl + "/mutate", { operations: this.wireRepresentation() })
         .onSuccess(function(responseData) {
           this.handleSuccessfulResponse(responseData);
