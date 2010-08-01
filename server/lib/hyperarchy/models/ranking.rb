28079bf46c2f90306470542c85451b4b0d6a283e
Nathan Sobo <nathansobo@gmail.com>
2010-08-01 15:31:09 -0300

    Negatively ranked candidates now lose to unranked candidates

--- server/lib/hyperarchy/models/ranking.rb
+++ server/lib/hyperarchy/models/ranking.rb
@@ -9,69 +9,100 @@ class Ranking < Monarch::Model::Record
   belongs_to :election
 
   def after_create
-    majorities_where_ranked_candidate_is_winner.
-      left_join(higher_rankings_by_same_user).on(:loser_id => :candidate_id).
-      where(:candidate_id => nil).
-      increment(:count)
-
-    lower_rankings_by_same_user.
-      join(majorities_where_ranked_candidate_is_loser).on(:candidate_id => :winner_id).
-      decrement(:count)
+    if position > 0
+      increment_victories_over(lower_positive_rankings_by_same_user)
+      decrement_defeats_by(lower_positive_rankings_by_same_user)
+      increment_victories_over(candidates_not_ranked_by_same_user)
+    else
+      increment_defeats_by(higher_negative_rankings_by_same_user)
+      decrement_victories_over(higher_negative_rankings_by_same_user)
+      increment_defeats_by(candidates_not_ranked_by_same_user)
+    end
 
     election.compute_global_ranking
   end
 
   def after_update(changeset)
     return unless changeset.changed?(:position)
-
     old_position = changeset.old_state.position
-    new_position = changeset.new_state.position
-    if new_position > old_position
-      after_ranking_moved_up(new_position, old_position)
+    if position > old_position
+      after_ranking_moved_up(old_position)
     else
-      after_ranking_moved_down(new_position, old_position)
+      after_ranking_moved_down(old_position)
     end
 
     election.compute_global_ranking
   end
 
-  def after_ranking_moved_up(new_position, old_position)
+  def after_ranking_moved_up(old_position)
     previously_higher_rankings = lower_rankings_by_same_user.where(Ranking[:position] > old_position)
-    previously_higher_rankings.
-      join(majorities_where_ranked_candidate_is_loser).on(:winner_id => :candidate_id).
-      decrement(:count)
 
-    previously_higher_rankings.
-      join(majorities_where_ranked_candidate_is_winner).on(:loser_id => :candidate_id).
-      increment(:count)
+    increment_victories_over(previously_higher_rankings)
+    decrement_defeats_by(previously_higher_rankings)
+
+    if position > 0 && old_position < 0
+      increment_victories_over(candidates_not_ranked_by_same_user)
+      decrement_defeats_by(candidates_not_ranked_by_same_user)
+    end
   end
 
-  def after_ranking_moved_down(new_position, old_position)
+  def after_ranking_moved_down(old_position)
     previously_lower_rankings = higher_rankings_by_same_user.where(Ranking[:position] < old_position)
-    previously_lower_rankings.
-      join(majorities_where_ranked_candidate_is_loser).on(:winner_id => :candidate_id).
-      increment(:count)
+    decrement_victories_over(previously_lower_rankings)
+    increment_defeats_by(previously_lower_rankings)
 
-    previously_lower_rankings.
-      join(majorities_where_ranked_candidate_is_winner).on(:loser_id => :candidate_id).
-      decrement(:count)
+    if position < 0 && old_position > 0
+      decrement_victories_over(candidates_not_ranked_by_same_user)
+      increment_defeats_by(candidates_not_ranked_by_same_user)
+    end
   end
 
   def after_destroy
-    majorities_where_ranked_candidate_is_winner.
-      left_join(higher_rankings_by_same_user).on(:loser_id => :candidate_id).
-      where(:candidate_id => nil).
-      decrement(:count)
-
-    lower_rankings_by_same_user.
-      join(majorities_where_ranked_candidate_is_loser).on(:winner_id => :candidate_id).
-      increment(:count)
+    if position > 0
+      decrement_victories_over(lower_positive_rankings_by_same_user)
+      increment_defeats_by(lower_positive_rankings_by_same_user)
+      decrement_victories_over(candidates_not_ranked_by_same_user)
+    else
+      increment_victories_over(higher_negative_rankings_by_same_user)
+      decrement_defeats_by(higher_negative_rankings_by_same_user)
+      decrement_defeats_by(candidates_not_ranked_by_same_user)
+    end
 
     if all_rankings_for_same_candidate.empty?
       candidate.update(:position => nil)
     end
 
     election.compute_global_ranking
+  end
+
+  def increment_victories_over(rankings_or_candidates)
+    victories_over(rankings_or_candidates).increment(:count)
+  end
+
+  def decrement_victories_over(rankings_or_candidates)
+    victories_over(rankings_or_candidates).decrement(:count)
+  end
+
+  def increment_defeats_by(rankings_or_candidates)
+    defeats_by(rankings_or_candidates).increment(:count)
+  end
+
+  def decrement_defeats_by(rankings_or_candidates)
+    defeats_by(rankings_or_candidates).decrement(:count)
+  end
+
+  def victories_over(rankings_or_candidates)
+    majorities_where_ranked_candidate_is_winner.
+      join(rankings_or_candidates).on(:loser_id => candidate_id_join_column(rankings_or_candidates))
+  end
+
+  def defeats_by(rankings_or_candidates)
+    majorities_where_ranked_candidate_is_loser.
+      join(rankings_or_candidates).on(:winner_id => candidate_id_join_column(rankings_or_candidates))
+  end
+
+  def candidate_id_join_column(rankings_or_candidates)
+    rankings_or_candidates.column(:candidate_id) ? :candidate_id : Candidate[:id]
   end
 
   def rankings_by_same_user
@@ -86,6 +117,30 @@ class Ranking < Monarch::Model::Record
     rankings_by_same_user.where(Ranking[:position] < position)
   end
 
+  def positive_rankings_by_same_user
+    rankings_by_same_user.where(Ranking[:position] > 0)
+  end
+
+  def negative_rankings_by_same_user
+    rankings_by_same_user.where(Ranking[:position] < 0)
+  end 
+
+  def higher_positive_rankings_by_same_user
+    positive_rankings_by_same_user.where(Ranking[:position] > position)
+  end
+
+  def lower_positive_rankings_by_same_user
+    positive_rankings_by_same_user.where(Ranking[:position] < position)
+  end
+
+  def higher_negative_rankings_by_same_user
+    negative_rankings_by_same_user.where(Ranking[:position] > position)
+  end
+
+  def lower_negative_rankings_by_same_user
+    negative_rankings_by_same_user.where(Ranking[:position] < position)
+  end
+
   def majorities_where_ranked_candidate_is_winner
     Majority.where(:winner_id => candidate_id)
   end
@@ -96,5 +151,16 @@ class Ranking < Monarch::Model::Record
 
   def all_rankings_for_same_candidate
     Ranking.where(:candidate_id => candidate_id)
+  end
+
+  def all_candidates_in_election
+    Candidate.where(:election_id => election_id)
+  end
+
+  def candidates_not_ranked_by_same_user
+    all_candidates_in_election.
+      left_join_to(rankings_by_same_user).
+      where(Ranking[:id] => nil).
+      project(Candidate)
   end
 end
--- server/spec/hyperarchy/models/ranking_spec.rb
+++ server/spec/hyperarchy/models/ranking_spec.rb
@@ -1,6 +1,4 @@
-require File.expand_path("#{File.dirname(__FILE__)}/../../hyperarchy_spec_helper")
-
-
+require File.expand_path(File.dirname(__FILE__) + "/../../hyperarchy_spec_helper")
 
 module Models
   describe Ranking do
@@ -20,18 +18,21 @@ module Models
           majority.count.should == 0
         end
 
-        # 1
+        # 1, (2, 3)
         mock.proxy(election).compute_global_ranking
         ranking_1 = election.rankings.create(:user => user, :candidate => candidate_1, :position => 64)
         find_majority(candidate_1, candidate_2).count.should == 1
         find_majority(candidate_1, candidate_3).count.should == 1
 
-        # 1, 2
+        # 1, 2, (3)
         mock.proxy(election).compute_global_ranking
         ranking_2 = election.rankings.create(:user => user, :candidate => candidate_2, :position => 32)
         find_majority(candidate_1, candidate_2).count.should == 1
+        find_majority(candidate_1, candidate_3).count.should == 1
         find_majority(candidate_2, candidate_1).count.should == 0
         find_majority(candidate_2, candidate_3).count.should == 1
+        find_majority(candidate_3, candidate_1).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 0
 
         # 1, 3, 2
         mock.proxy(election).compute_global_ranking
@@ -47,9 +48,11 @@ module Models
         mock.proxy(election).compute_global_ranking
         ranking_2.update(:position => 56)
         find_majority(candidate_1, candidate_2).count.should == 1
+        find_majority(candidate_1, candidate_3).count.should == 1
         find_majority(candidate_2, candidate_1).count.should == 0
         find_majority(candidate_2, candidate_3).count.should == 1
         find_majority(candidate_3, candidate_2).count.should == 0
+        find_majority(candidate_3, candidate_1).count.should == 0
 
         # 2, 1, 3
         mock.proxy(election).compute_global_ranking
@@ -57,7 +60,9 @@ module Models
         find_majority(candidate_1, candidate_2).count.should == 0
         find_majority(candidate_1, candidate_3).count.should == 1
         find_majority(candidate_2, candidate_1).count.should == 1
+        find_majority(candidate_2, candidate_3).count.should == 1
         find_majority(candidate_3, candidate_1).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 0
 
         # 3, 2, 1
         mock.proxy(election).compute_global_ranking
@@ -69,7 +74,7 @@ module Models
         find_majority(candidate_3, candidate_1).count.should == 1
         find_majority(candidate_3, candidate_2).count.should == 1
 
-        # 3, 1
+        # 3, 1, (2)
         mock.proxy(election).compute_global_ranking
         ranking_2.destroy
         find_majority(candidate_1, candidate_2).count.should == 1
@@ -79,7 +84,7 @@ module Models
         find_majority(candidate_3, candidate_1).count.should == 1
         find_majority(candidate_3, candidate_2).count.should == 1
 
-        # 1
+        # 1, (2, 3)
         mock.proxy(election).compute_global_ranking
         ranking_3.destroy
         find_majority(candidate_1, candidate_2).count.should == 1
@@ -100,6 +105,82 @@ module Models
         candidate_1.position.should be_nil
         candidate_3.position.should be_nil
         candidate_3.position.should be_nil
+      end
+
+      specify "negatively ranked candidates are counted as losing to unranked candidates" do
+        election.majorities.each do |majority|
+          majority.count.should == 0
+        end
+
+        # (2, 3), 1
+        mock.proxy(election).compute_global_ranking
+        ranking_1 = election.rankings.create(:user => user, :candidate => candidate_1, :position => -64)
+        find_majority(candidate_1, candidate_2).count.should == 0
+        find_majority(candidate_1, candidate_3).count.should == 0
+        find_majority(candidate_2, candidate_1).count.should == 1
+        find_majority(candidate_2, candidate_3).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 0
+        find_majority(candidate_3, candidate_1).count.should == 1
+
+        # (3), 1, 2
+        mock.proxy(election).compute_global_ranking
+        ranking_2 = election.rankings.create(:user => user, :candidate => candidate_2, :position => -128)
+        find_majority(candidate_1, candidate_2).count.should == 1
+        find_majority(candidate_1, candidate_3).count.should == 0
+        find_majority(candidate_2, candidate_1).count.should == 0
+        find_majority(candidate_2, candidate_3).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 1
+        find_majority(candidate_3, candidate_1).count.should == 1
+
+        # (3), 2, 1
+        mock.proxy(election).compute_global_ranking
+        ranking_2.update(:position => -32)
+        find_majority(candidate_1, candidate_2).count.should == 0
+        find_majority(candidate_1, candidate_3).count.should == 0
+        find_majority(candidate_2, candidate_1).count.should == 1
+        find_majority(candidate_2, candidate_3).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 1
+        find_majority(candidate_3, candidate_1).count.should == 1
+
+        # 1, (3), 2
+        mock.proxy(election).compute_global_ranking
+        ranking_1.update(:position => 64)
+        find_majority(candidate_1, candidate_2).count.should == 1
+        find_majority(candidate_1, candidate_3).count.should == 1
+        find_majority(candidate_2, candidate_1).count.should == 0
+        find_majority(candidate_2, candidate_3).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 1
+        find_majority(candidate_3, candidate_1).count.should == 0
+
+        # (3), 1, 2
+        mock.proxy(election).compute_global_ranking
+        ranking_1.update(:position => -16)
+        find_majority(candidate_1, candidate_2).count.should == 1
+        find_majority(candidate_1, candidate_3).count.should == 0
+        find_majority(candidate_2, candidate_1).count.should == 0
+        find_majority(candidate_2, candidate_3).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 1
+        find_majority(candidate_3, candidate_1).count.should == 1
+
+        # (3, 2), 1
+        mock.proxy(election).compute_global_ranking
+        ranking_2.destroy
+        find_majority(candidate_1, candidate_2).count.should == 0
+        find_majority(candidate_1, candidate_3).count.should == 0
+        find_majority(candidate_2, candidate_1).count.should == 1
+        find_majority(candidate_2, candidate_3).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 0
+        find_majority(candidate_3, candidate_1).count.should == 1
+
+        # (1, 2, 3) -- all unranked
+        mock.proxy(election).compute_global_ranking
+        ranking_1.destroy
+        find_majority(candidate_1, candidate_2).count.should == 0
+        find_majority(candidate_1, candidate_3).count.should == 0
+        find_majority(candidate_2, candidate_1).count.should == 0
+        find_majority(candidate_2, candidate_3).count.should == 0
+        find_majority(candidate_3, candidate_2).count.should == 0
+        find_majority(candidate_3, candidate_1).count.should == 0
       end
     end
   end
