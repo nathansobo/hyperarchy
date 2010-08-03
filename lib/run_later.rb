1f29c8b85483aa25941412b28a7929e777f77780
Jens Kraemer <jk@jkraemer.net>
2010-08-03 19:02:22 +0200

    port to rails3

--- lib/run_later.rb
+++ lib/run_later.rb
@@ -2,6 +2,10 @@ require 'run_later/worker'
 require 'run_later/instance_methods'
 require 'run_later/cleanup'
 
+module RunLater
+  require 'run_later/railtie' if defined?(Rails)
+end
+
 ActionController::Base.send(:include, RunLater::InstanceMethods)
 # Make run_later available both as instance and class methods
 ActiveRecord::Base.send(:include, RunLater::InstanceMethods)
--- lib/run_later/cleanup.rb
+++ lib/run_later/cleanup.rb
@@ -1,12 +1,9 @@
-require 'dispatcher' unless defined?(::Dispatcher)
-
-# Cleanup code. Will only be used when cache_classes is disabled, so usually
-# in development and testing mode. Ensures that the worker thread is properly
-# disposed of before Rails' class unloading kicks in.
-
-if defined?(ActionController::MiddlewareStack::Middleware)
-  # Rack-supported version for Rails >= 2.3.x
-  class RunLater::Cleanup
+module RunLater
+  
+  # Cleanup code. Will only be used when cache_classes is disabled, so usually
+  # in development and testing mode. Ensures that the worker thread is properly
+  # disposed of before Rails' class unloading kicks in.
+  class Cleanup
     def initialize(app)
       @app = app
     end
@@ -18,17 +15,4 @@ if defined?(ActionController::MiddlewareStack::Middleware)
     end
   end
 
-  unless Rails.configuration.cache_classes
-    ActionController::Dispatcher.middleware.push(ActionController::MiddlewareStack::Middleware.new(RunLater::Cleanup))
-  end
-else
-  # "Legacy" Rails 2.2 version
-  class ActionController::Dispatcher
-    def cleanup_application_with_thread_check
-      RunLater::Worker.cleanup
-      cleanup_application_without_thread_check
-    end
-
-    alias_method_chain :cleanup_application, :thread_check
-  end
 end
