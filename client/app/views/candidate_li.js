17753faf9de0dcbb263a4bae6e2f3d207d0aeae2
Nathan Sobo <nathansobo@gmail.com>
2010-08-22 21:06:44 -0500

    Clicking the arrow expands the answer, revealing a details box

--- client/app/views/candidate_li.js
+++ client/app/views/candidate_li.js
@@ -2,9 +2,30 @@ _.constructor("Views.CandidateLi", View.Template, {
   content: function(params) { with(this.builder) {
     var candidate = params.candidate;
     li({ candidateId: candidate.id(), 'class': "candidate " + this.additionalClass }, function() {
-      div({'class': "expandArrow closed"});
+      div({'class': "expandArrow"})
+        .ref('expandArrow')
+        .click('expandOrContract');
       template.candidateIcon();
       span({'class': "body"}, candidate.body());
+
+      div({'class': "expandedInfo", style: "display: none;"}, function() {
+        label({'for': "candidateDetails"}, "Details:");
+        textarea({name: "candidateDetails"});
+      }).ref('expandedInfo');
     });
-  }}
+  }},
+
+  viewProperties: {
+    expandOrContract: function() {
+      if (this.expanded) {
+        this.expanded = false;
+        this.expandArrow.removeClass('expanded');
+        this.expandedInfo.slideUp('fast');
+      } else {
+        this.expanded = true;
+        this.expandArrow.addClass('expanded');
+        this.expandedInfo.slideDown('fast');
+      }
+    }
+  }
 });
