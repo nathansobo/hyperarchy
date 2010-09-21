26e1ae65e66bd45b5e4dfc9fc3fbb023c362a584
Nathan Sobo <nathansobo@gmail.com>
2010-09-21 18:09:30 +0530

    Added greater than and less than predicates to client side

--- server/vendor/monarch/client/lib/monarch/model/predicates.js
+++ server/vendor/monarch/client/lib/monarch/model/predicates.js
@@ -4,6 +4,10 @@
 //= require "predicates/binary"
 //= require "predicates/eq"
 //= require "predicates/neq"
+//= require "predicates/gt"
+//= require "predicates/gte"
+//= require "predicates/lt"
+//= require "predicates/lte"
 //= require "predicates/and"
 
 })(Monarch);
