e709acb081e2d011acd3e1a3ffae9af62787c005
Nathan Sobo <nathansobo@gmail.com>
2010-12-21 20:59:06 -0800

    Putting chrome frame install code in window.onload

--- server/lib/hyperarchy/views/no_internet_explorer.rb
+++ server/lib/hyperarchy/views/no_internet_explorer.rb
@@ -9,7 +9,9 @@ module Views
 
     def body_content
       script :type => "text/javascript", :src => "http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"
-      javascript %[CFInstall.check({mode: "overlay"})]
+      javascript %[
+        window.onload = function() { CFInstall.check({mode: "overlay"}); }
+      ]
       h1 "Sorry. We don't support internet explorer yet."
     end
   end
