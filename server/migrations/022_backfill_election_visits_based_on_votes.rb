055370c83e886bbe809894aafb31b8e6a33b198d
Nathan Sobo <nathansobo@gmail.com>
2010-12-24 14:34:38 -0800

    Bad migration tried to insert a specific id.

--- server/migrations/022_backfill_election_visits_based_on_votes.rb
+++ server/migrations/022_backfill_election_visits_based_on_votes.rb
@@ -1,6 +1,7 @@
 Sequel.migration do
   up do
     self[:votes].each do |vote_field_values|
+      vote_field_values.delete(:id)
       self[:election_visits].insert(vote_field_values)
     end
   end
