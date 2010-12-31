10c084b9b7d4e5e008a8194893d4bca88274086f
Nathan Sobo <nathansobo@gmail.com>
2010-12-30 17:37:22 -0800

    Factored stairstep logic into a single method that optionally updates a passed-in next array

--- server/vendor/monarch/client/lib/monarch/skip_list.js
+++ server/vendor/monarch/client/lib/monarch/skip_list.js
@@ -29,58 +29,33 @@ _.constructor("Monarch.SkipList", {
 
   insert: function(key, value) {
     if (!value) value = key;
-
-    var cursor = this.head;
     var next = new Array(this.maxLevels);
-
-    // search the skiplist, following the longest link for max efficiency
-    for (var i = this.currentLevel; i >= 0; i--) {
-      // move forward as far as possible while keeping the cursor node's key less than the inserted key
-      while (this.compare(cursor.pointer[i].key, key) < 0) {
-        cursor = cursor.pointer[i];
-      }
-      // when the next link would be bigger than our key, drop a level
-      // before we do, note that this is the last node we visited at level i
-      next[i] = cursor;
-    }
-
-    // advance to the node we're going to precede or replace
-    cursor = cursor.pointer[0];
+    var closestNode = this.findClosestNode(key, next);
 
     // if the key is a duplicate, replace its value. otherwise insert a node
-    if (cursor.key == key) {
-      cursor.value = value;
+    if (closestNode.key == key) {
+      closestNode.value = value;
     } else {
       var level = this.randomLevel();
 
-      // if the overall level in increasing, set the new level fill in the next array with this.head for the new levels
+      // if the overall level in increasing, set the new level and fill in the next array with this.head for the new levels
       if (level > this.currentLevel) {
         for (i = this.currentLevel + 1; i <= level; i++) next[i] = this.head;
         this.currentLevel = level;
       }
 
       // create a new node and insert it by updating pointers at every level
-      cursor = new Monarch.SkipListNode(level, key, value);
+      closestNode = new Monarch.SkipListNode(level, key, value);
       for (i = 0; i <= level; i++) {
-        cursor.pointer[i] = next[i].pointer[i];
-        next[i].pointer[i] = cursor;
+        closestNode.pointer[i] = next[i].pointer[i];
+        next[i].pointer[i] = closestNode;
       }
     }
   },
 
   remove: function(key) {
-    var cursor = this.head;
     var next = new Array(this.maxLevels);
-
-    for (var i = this.currentLevel; i >= 0; i--) {
-      var nextNode = cursor.pointer[i];
-      while (this.compare(nextNode.key, key) === -1) {
-        cursor = nextNode;
-        nextNode = cursor.pointer[i];
-      }
-      next[i] = cursor;
-    }
-    cursor = cursor.pointer[0];
+    var cursor = this.findClosestNode(key, next);
 
     if (this.compare(cursor.key, key) === 0) {
       for (i = 0; i <= this.currentLevel; i++) {
@@ -98,20 +73,7 @@ _.constructor("Monarch.SkipList", {
   },
 
   find: function(key) {
-    // start at the head and stairstep downward until the we can't advance any further without being greater than the search key
-    var cursor = this.head;
-    for (var i = this.currentLevel; i >= 0; i--) {
-      var nextNode = cursor.pointer[i];
-      // advance as far as possible at the current level without overshooting (having the node's key be less than the search key)
-      while(this.compare(nextNode.key, key) === -1) {
-        cursor = nextNode;
-        nextNode = cursor.pointer[i];
-      }
-      // drop down if we overshot until we reach the bottom
-    }
-
-    // now advance one more. either this is the node we were seeking or it isn't in the list
-    cursor = cursor.pointer[0];
+    var cursor = this.findClosestNode(key, false);
     if (this.compare(cursor.key, key) === 0) {
       return cursor.value;
     } else {
@@ -130,6 +92,24 @@ _.constructor("Monarch.SkipList", {
   },
 
   // private
+
+  findClosestNode: function(key, next) {
+    // search the skiplist in a stairstep descent, following the highest path that doesn't overshoot the key
+    var cursor = this.head;
+    for (var i = this.currentLevel; i >= 0; i--) {
+      // move forward as far as possible while keeping the cursor node's key less than the inserted key
+      while (this.compare(cursor.pointer[i].key, key) < 0) {
+        cursor = cursor.pointer[i];
+      }
+      // when the next link would be bigger than our key, drop a level
+      // before we do, note that this is the last node we visited at level i in the next array
+      if (next) next[i] = cursor;
+    }
+
+    // advance to the next node... it is the nearest node whose key is >= the search key
+    return cursor.pointer[0];
+  },
+
 
   randomLevel: function() {
     var maxLevels = this.maxLevels;
