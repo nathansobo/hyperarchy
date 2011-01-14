a2a66d934ee8eb3642a0c1f1fd36f9a889003bc6
Nathan Sobo <nathansobo@gmail.com>
2011-01-13 21:54:33 -0800

    Switched to Rufus as scheduler. Installed hourly, daily, weekly alerts.

--- server/lib/hyperarchy/scheduler.rb
+++ server/lib/hyperarchy/scheduler.rb
@@ -3,15 +3,36 @@ module Hyperarchy
     extend self
 
     def start
-      Clockwork.every(1.minute, "update election scores") do
+      scheduler = Rufus::Scheduler.start_new
+      scheduler.every '1m' do
         Hyperarchy.defer do
+          puts "update scores"
           Election.update_scores
         end
       end
 
-      Thread.new do
-        Thread.abort_on_exception = true
-        Clockwork.run
+      # every hour
+      scheduler.cron '0 0 * * * *' do
+        Hyperarchy.defer do
+          puts "Sending hourly alerts"
+          Alerter.new.send_alerts(:hourly)
+        end
+      end
+
+      # 5:30 am daily PST
+      scheduler.cron '0 30 5 * * * America/Los_Angeles' do
+        Hyperarchy.defer do
+          puts "Sending daily alerts"
+          Alerter.new.send_alerts(:daily)
+        end
+      end
+
+      # 5:30 am on wednesday PST
+      scheduler.cron '0 30 5 * * 3 America/Los_Angeles' do
+        Hyperarchy.defer do
+          puts "Sending weekly alerts"
+          Alerter.new.send_alerts(:weekly)
+        end
       end
     end
   end
