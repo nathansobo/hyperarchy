273771c3f6f3eb65d1f84e56478dd1ad9925a57f
Nathan Sobo <nathansobo@gmail.com>
2011-01-18 15:03:51 -0800

    Each comment has a header with the creator name and time.

--- client/app/views/candidate_comment_li.js
+++ client/app/views/candidate_comment_li.js
@@ -2,14 +2,11 @@ _.constructor("Views.CandidateCommentLi", View.Template, {
   content: function(params) {with(this.builder) {
     var candidateComment = params.candidateComment;
     li({candidateCommentId: candidateComment.id(), 'class': "candidateComment"}, function() {
-      div({'class': "commentBody"}, candidateComment.body());
-
-      div({'class': "commentCreator"}, function() {
-        raw("&mdash;")
-        text(candidateComment.creator().fullName());
+      div({'class': "commentHeader"}, function() {
+        div({'class': "commentCreator"}, candidateComment.creator().fullName());
+        div({'class': "commentCreatedAt"}, candidateComment.formattedCreatedAt());
       });
-      div({'class': "commentCreatedAt"}, candidateComment.formattedCreatedAt());
-      div({'class': "clear"});
+      div({'class': "commentBody"}, candidateComment.body());
     });
   }}
 });
