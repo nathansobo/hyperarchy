2a6651403a64e433ef12315b13f9dd44df78ef32
Nathan Sobo <nathansobo@gmail.com>
2011-02-02 21:36:58 -0800

    Start experiment with rails 3 port

---
+++ hyperarchy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hyperarchy::Application.config.secret_token = 'ec0004ac9d371845169e01bdea3aa147ea024aa058bbff07613bbee4d46e94cf1991458b238722fc18ad7d077a6965c8b46fcbf797e0c1e691dc43787f73d8d9'
