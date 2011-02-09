285abd78617e37fd72a5356a1587d5637e8f42b9
Max Brunsfeld <maxbrunsfeld@gmail.com>
2011-02-08 20:09:31 -0800

    migration adds timezones to all timestamp columns in the database

---
+++ server/migrations/032_add_timezones.rb
@@ -0,0 +1,39 @@
+Sequel.migration do
+  up do
+
+    # find all columns of type :datetime
+    time_columns = {}
+    tables.each do |table|
+      time_columns[table] =
+        schema(table).select{|column| column[1][:type] == :datetime}.map{|column| column[0]}
+    end
+    p time_columns.inspect
+
+    tables.each do |table|
+      time_columns[table].each do |column|
+        alter_table table do
+          set_column_type column, 'timestamp with time zone'
+        end
+      end
+    end
+  end
+
+  down do
+
+    # find all columns of type :datetime
+    time_columns = {}
+    tables.each do |table|
+      time_columns[table] =
+        schema(table).select{|column| column[1][:type] == :datetime}.map{|column| column[0]}
+    end
+    p time_columns.inspect
+    
+    tables.each do |table|
+      time_columns[table].each do |column|
+        alter_table table do
+          set_column_type column, 'timestamp with time zone'
+        end
+      end
+    end
+  end
+end
