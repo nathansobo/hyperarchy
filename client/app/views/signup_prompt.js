072ec4ca1e320bfeeaca369affc682bc2a134733
Nathan Sobo <nathansobo@gmail.com>
2011-02-17 23:16:50 -0800

    Guard the signup prompt from triggering success on a future when it is null

--- client/app/views/signup_prompt.js
+++ client/app/views/signup_prompt.js
@@ -67,7 +67,10 @@ _.constructor("Views.SignupPrompt", View.Template, {
       this.signupForm.show();
       this.loginForm.hide();
       this.find('input[type="text"]').val("")
-      if (this.future) this.future.triggerFailure();
+      if (this.future) {
+        this.future.triggerFailure();
+        delete this.future;
+      } 
     },
 
     toggleForms: function() {
@@ -106,8 +109,10 @@ _.constructor("Views.SignupPrompt", View.Template, {
 
     userEstablished: function(data) {
       Application.currentUserIdEstablished(data.current_user_id)
-      this.future.triggerSuccess();
-      delete this.future;
+      if (this.future) {
+        this.future.triggerSuccess();
+        delete this.future;
+      }
       this.hide();
     },
 
