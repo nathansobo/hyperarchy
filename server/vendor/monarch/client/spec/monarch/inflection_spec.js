1fe48c537ab6040f2e6dd2bbce6cc939707c1a70
Max Brunsfeld <maxbrunsfeld@gmail.com>
2011-02-22 02:06:57 -0800

    fix bugs, add specs for _.singularize in inflection.js

--- server/vendor/monarch/client/spec/monarch/inflection_spec.js
+++ server/vendor/monarch/client/spec/monarch/inflection_spec.js
@@ -8,5 +8,19 @@ Screw.Unit(function(c) { with(c) {
         expect(_.humanize("FastCars")).to(eq, "Fast Cars");
       });
     });
+
+    describe(".singularize", function() {
+      it("leaves singular words unchanged", function() {
+        expect(_.singularize("cars")).to(eq, "car");
+        expect(_.singularize("car")).to(eq,  "car");
+      });
+
+      it("returns the singular form of irregular plural words", function() {
+        expect(_.singularize("people")).to(eq, "person");
+        expect(_.singularize("People")).to(eq, "Person");
+        expect(_.singularize("person")).to(eq, "person");
+        expect(_.singularize("Person")).to(eq, "Person");
+      });
+    });
   });
 }});
