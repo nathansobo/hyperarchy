98e9901b417cd885c8cdfa3c32d0a8e8a9230596
Nathan Sobo <nathansobo@gmail.com>
2011-04-11 13:12:40 -0700

    Add a dummy implementation of defer for now

--- hyperarchy/config/application.rb
+++ hyperarchy/config/application.rb
@@ -11,6 +11,10 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 require 'erector/rails/railtie'
 
 module Hyperarchy
+  def self.defer
+    yield
+  end
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
