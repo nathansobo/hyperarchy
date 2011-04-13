330db216d1075a73f4ad9887f50da7269bb602d8
Nathan Sobo <nathansobo@gmail.com>
2011-04-13 00:07:17 -0700

    In progress: Notify existing users when they are added to organizations
    Still have to make the email body with the hash-fragment url. Not sure
    what kind of support there is for these urls in rails 3.

--- hyperarchy/spec/mailers/membership_mailer_spec.rb
+++ hyperarchy/spec/mailers/membership_mailer_spec.rb
@@ -8,11 +8,10 @@ describe MembershipMailer do
     before do
       @organization = Organization.make
       set_current_user(make_member(organization))
-      @membership = organization.memberships.create!(
+      @membership = organization.memberships.make(
         :first_name => "John",
         :last_name => "Doe",
-        :email_address => "johndoe@example.com",
-        :suppress_invite_email => true
+        :email_address => "johndoe@example.com"
       )
       @invitation = membership.invitation
 
@@ -33,6 +32,41 @@ describe MembershipMailer do
     it "includes a link to the signup page with an invitation parameter in both the html and plain-text portions of the email" do
       email.text_part.body.should include(signup_url(:invitation_code => invitation.guid))
       email.html_part.body.should include(signup_url(:invitation_code => invitation.guid))
+    end
+  end
+
+  describe "#existing_user_notification" do
+    attr_reader :organization, :membership, :invitation, :email
+
+    before do
+      user = User.make
+      @organization = Organization.make
+      set_current_user(make_member(organization))
+      @membership = organization.memberships.make(
+        :first_name => user.first_name,
+        :last_name => user.last_name,
+        :email_address => user.email_address
+      )
+      membership.user.should == user
+
+      @email = MembershipMailer.existing_user_notification(current_user.id, membership.id).deliver
+      ActionMailer::Base.deliveries.should_not be_empty
+    end
+
+    it "is from admin@hyperarchy.com, to the user that was added as a member" do
+      email.from.should == ["admin@hyperarchy.com"]
+      email.to.should == [membership.email_address]
+    end
+
+    it "mentions the sender's name and organization in the subject" do
+      email.subject.should include(current_user.full_name)
+      email.subject.should include(organization.name)
+    end
+
+    it "includes a link to the organization in the signup page" do
+      pending
+      email.text_part.body.should include(home_url)
+      email.html_part.body.should include(signup_url)
     end
   end
 end
