5b93c1d4060f0ea3c6afb5c851156fb9a1786b3b
Nathan Sobo <nathansobo@gmail.com>
2011-05-02 22:45:32 -0700

    Use default bundler cap stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
+require "bundler/capistrano"
 require 'capistrano/ext/multistage'
 
 set :application, "hyperarchy"
@@ -14,42 +15,14 @@ ssh_options[:keys] = [File.expand_path('config/provision/keys/id_rsa')]
 ssh_options[:forward_agent] = true
 
 namespace :deploy do
-  task :restart, :roles => :app do
+  task :restart do
     # restart unicorn here
   end
 end
 
-namespace :bundler do
-  task :create_symlink, :roles => :app do
-    shared_dir = File.join(shared_path, 'bundle')
-    release_dir = File.join(release_path, '.bundle')
-    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
-  end
-
-  task :install, :roles => :app do
-    run "cd #{release_path} && bundle install --deployment --without development test"
-
-    on_rollback do
-      if previous_release
-        run "cd #{previous_release} && bundle install --deployment --without development test"
-      else
-        logger.important "no previous release to rollback to, rollback of bundler:install skipped"
-      end
-    end
-  end
-
-  task :bundle_new_release, :roles => :db do
-    bundler.create_symlink
-    bundler.install
-  end
-end
-
+before :deploy, :add_ssh_agent
 task :add_ssh_agent do
   unless run_locally("ssh-add -l") =~ %r{config/provision/keys/id_rsa}
     run_locally("ssh-add #{ssh_options[:keys].first}")
   end
 end
-
-before "deploy", "add_ssh_agent"
-after "deploy:rollback:revision", "bundler:install"
-after "deploy:update_code", "bundler:bundle_new_release"
