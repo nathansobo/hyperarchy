ac1e7f2096cf6cc22625c0d1358b989dd1e4007a
Nathan Sobo <nathansobo@gmail.com>
2011-05-04 19:09:09 -0700

    move service definitions to /config/deploy/services in the repo

--- config/deploy/services.rb
+++ config/deploy/services.rb
@@ -10,10 +10,10 @@ namespace :services do
     task :unicorn do
       require 'erb'
       run 'mkdir -p /var/svc.d/unicorn/log'
-      upload 'config/deploy/service/unicorn/run', '/var/svc.d/unicorn/run', :mode => 0755
-      template = ERB.new(File.read('config/deploy/service/unicorn/unicorn.sh.erb'))
+      upload 'config/deploy/services/unicorn/run', '/var/svc.d/unicorn/run', :mode => 0755
+      template = ERB.new(File.read('config/deploy/services/unicorn/unicorn.sh.erb'))
       put template.result(binding), '/var/svc.d/unicorn/unicorn.sh', :mode => 0755
-      upload 'config/deploy/service/unicorn/log/run', '/var/svc.d/unicorn/log/run', :mode => 0755
+      upload 'config/deploy/services/unicorn/log/run', '/var/svc.d/unicorn/log/run', :mode => 0755
       run 'ln -s /var/svc.d/unicorn /service/unicorn'
     end
   end
