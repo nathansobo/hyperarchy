87e0efb368eb52041ea3f59f4c9e06b7e2760bb6
Max Brunsfeld & Nathan Sobo <nathansobo@gmail.com>
2011-05-25 10:56:35 -0700

    Add more logging to send notifications

--- app/jobs/send_notifications.rb
+++ app/jobs/send_notifications.rb
@@ -3,12 +3,13 @@ module Jobs
     @queue = 'send_notifications'
 
     def perform
+      puts "Sending notifications for period #{period}"
       users_to_notify = User.users_to_notify(period)
       total = users_to_notify.count
       users_to_notify.each_with_index do |user, i|
         at(i + 1, total)
+        puts "Considering #{user.email_address} for notification"
         presenter = Views::NotificationMailer::NotificationPresenter.new(user, period)
-
         unless presenter.empty?
           puts "Sending notification to #{user.email_address} -- #{presenter.item_counts}"
           NotificationMailer.notification(user, presenter).deliver
