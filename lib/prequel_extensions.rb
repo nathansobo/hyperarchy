4698db7d6583e55c83c495dd30522cc5234d9d75
Nathan Sobo <nathansobo@gmail.com>
2011-05-31 11:29:33 -0700

    Move validations into main prequel lib

--- lib/prequel_extensions.rb
+++ lib/prequel_extensions.rb
@@ -4,21 +4,6 @@ module Prequel
   end
 
   class Record
-    class << self
-      def validates_uniqueness_of(column_name, options={})
-        column = table.get_column(column_name)
-        raise "No column #{column_name.inspect} to validate the uniqueness of" unless column
-        validate do
-          field_value = get_field_value(column_name)
-          relation = table.where(column_name => field_value)
-          relation = relation.where(:id.neq(id)) if persisted?
-          unless relation.empty?
-            errors.add(column_name, options[:message] || "#{column_name.to_s.humanize} must be unique")
-          end
-        end
-      end
-    end
-
     def current_user
       Prequel.session.current_user
     end
