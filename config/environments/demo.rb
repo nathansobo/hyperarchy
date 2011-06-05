97ae33dae2b7768c54dcb7d05d83860d69eee43a
Nathan Sobo <nathansobo@gmail.com>
2011-06-05 14:31:19 -0700

    Turn minification back on to see what happens

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -24,8 +24,8 @@ Hyperarchy::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Compress both stylesheets and JavaScripts
-#  config.assets.js_compressor  = :uglifier
-#  config.assets.css_compressor = :scss
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :scss
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
