961c0955bc0fdf75b31acc72d9cc0fc3f54e7530
Max Brunsfeld & Nathan Sobo <nathansobo@gmail.com>
2011-06-13 14:16:26 -0700

    Disable $redis.lock/unlock in jasmine mode

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,11 @@
 $redis = Redis.new
+
+if Rails.env.jasmine?
+  def $redis.lock(name)
+    puts "Stubbed lock acquisition: #{name}"
+  end
+
+  def $redis.unlock(name)
+    puts "Stubbed lock release: #{name}"
+  end
+end
