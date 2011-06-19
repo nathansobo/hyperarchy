85f275a1b5e10ba605d9fcb142d625af03a24857
Max Brunsfeld & Nathan Sobo <nathansobo@gmail.com>
2011-06-18 18:03:04 -0700

    style candidate lis with numbers, still a work in progress

--- app/assets/javascripts/views/pages/election/candidate_li.js
+++ app/assets/javascripts/views/pages/election/candidate_li.js
@@ -1,6 +1,9 @@
 _.constructor('Views.Pages.Election.CandidateLi', Monarch.View.Template, {
   content: function(params) { with(this.builder) {
-    li({'class': "candidate"}, params.candidate.body()).click(function() {
+    li({'class': "candidate"}, function() {
+      div({'class': "position"}, params.candidate.position());
+      div({'class': "body"}, params.candidate.body());
+    }).click(function() {
       History.pushState(null, null, params.candidate.url());
     });
   }},
