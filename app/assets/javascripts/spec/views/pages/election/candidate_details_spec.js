532e826fbdb468b9aa931820c1d8a527c2aba06c
Max Brunsfeld & Nathan Sobo <nathansobo@gmail.com>
2011-06-20 11:29:44 -0700

    Test coverage on showing candidate details form.

--- app/assets/javascripts/spec/views/pages/election/candidate_details_spec.js
+++ app/assets/javascripts/spec/views/pages/election/candidate_details_spec.js
@@ -4,18 +4,78 @@ describe("Views.Pages.Election.CandidateDetails", function() {
 
   var candidateDetails, candidate;
   beforeEach(function() {
-    candidateDetails = Views.Pages.Election.CandidateDetails.toView();
+    attachLayout();
+    candidateDetails = Application.electionPage.candidateDetails;
     candidate = Candidate.createFromRemote({id: 1, body: "Mustard.", details: "Pardon me. Do you have any Gray Poupon?"});
+    $('#jasmine_content').html(Application.electionPage);
+    Application.electionPage.showCandidateDetails();
+
+    candidateDetails.candidate(candidate);
   });
 
   describe("when the candidate is assigned", function() {
     it("populates the body and details divs and keeps them updated", function() {
-      candidateDetails.candidate(candidate);
       expect(candidateDetails.body.text()).toEqual(candidate.body());
       expect(candidateDetails.details.text()).toEqual(candidate.details());
       candidate.remotelyUpdated({body: "Catsup", details: "37 flavors"});
       expect(candidateDetails.body.text()).toEqual(candidate.body());
       expect(candidateDetails.details.text()).toEqual(candidate.details());
+    });
+  });
+  
+  describe("showing and hiding of the edit form", function() {
+    it("shows and populates the form fields when edit is clicked and hides them when cancel is clicked", function() {
+      expect(candidateDetails.form).toBeHidden();
+      expect(candidateDetails.nonEditableContent).toBeVisible();
+
+      candidateDetails.editLink.click();
+
+      expect(candidateDetails.form).toBeVisible();
+      expect(candidateDetails.nonEditableContent).toBeHidden();
+
+      expect(candidateDetails.formBody.val()).toBe(candidate.body());
+      expect(candidateDetails.formDetails.val()).toBe(candidate.details());
+
+      candidateDetails.cancelEditLink.click();
+
+      expect(candidateDetails.form).toBeHidden();
+      expect(candidateDetails.nonEditableContent).toBeVisible();
+    });
+  });
+
+  describe("when the save is button is clicked", function() {
+    var updates;
+
+    beforeEach(function() {
+      useFakeServer();
+      candidateDetails.editLink.click();
+      updates = {
+        body: "Relish",
+        details: "That green stuff..."
+      }
+
+      candidateDetails.formBody.val(updates.body);
+      candidateDetails.formDetails.val(updates.details);
+    });
+
+    it("updates the record's body and details on the server and hides the form", function() {
+      candidateDetails.saveLink.click();
+  
+      expect(Server.updates.length).toBe(1);
+
+      expect(Server.lastUpdate.dirtyFieldValues).toEqual(updates);
+      Server.lastUpdate.simulateSuccess();
+
+      expect(candidateDetails.form).toBeHidden();
+      expect(candidateDetails.nonEditableContent).toBeVisible();
+      
+      expect(candidateDetails.body.text()).toBe(updates.body);
+      expect(candidateDetails.details.text()).toBe(updates.details);
+    });
+
+    it("wires the form submit event to save", function() {
+      candidateDetails.form.submit();
+      expect(Server.updates.length).toBe(1);
     });
   });
 });
--- app/assets/javascripts/views/pages/election/candidate_details.js
+++ app/assets/javascripts/views/pages/election/candidate_details.js
@@ -1,18 +1,47 @@
 _.constructor('Views.Pages.Election.CandidateDetails', Monarch.View.Template, {
   content: function() { with(this.builder) {
     div({id: "candidate-details"}, function() {
-      div({'class': "body"}).ref("body");
-      div({'class': "details"}).ref("details");
+      div(function() {
+        div({'class': "body"}).ref("body");
+        div({'class': "details"}).ref("details");
+        a("Edit").ref('editLink').click('edit');
+      }).ref('nonEditableContent');
+
+      form(function() {
+        textarea({name: "body", 'class': "body"}).ref("formBody");
+        label({'for': "details"}, "Further Details");
+        textarea({name: 'details', 'class': "details"}).ref("formDetails");
+
+        a("Cancel").ref('cancelEditLink').click('hideForm');
+        a("Save").ref('saveLink').click('save');
+      }).submit('save')
+        .ref('form');
     });
   }},
 
   viewProperties: {
-
     candidate: {
       change: function(candidate) {
         this.body.bindText(candidate, 'body');
         this.details.bindText(candidate, 'details');
       }
+    },
+
+    edit: function() {
+      this.nonEditableContent.hide();
+      this.form.show();
+      this.formBody.val(this.candidate().body());
+      this.formDetails.val(this.candidate().details());
+    },
+
+    save: function(e) {
+      e.preventDefault();
+      this.candidate().update(this.form.fieldValues()).success(this.hitch('hideForm'));
+    },
+
+    hideForm: function() {
+      this.nonEditableContent.show();
+      this.form.hide();
     }
   }
 });
