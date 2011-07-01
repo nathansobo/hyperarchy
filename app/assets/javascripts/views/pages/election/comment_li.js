ed576a072feacf81cd82a3342477342a8e87f321
Max Brunsfeld & Nathan Sobo <nathansobo@gmail.com>
2011-07-01 16:28:08 -0700

    Replace image X with a character

--- app/assets/javascripts/views/pages/election/comment_li.js
+++ app/assets/javascripts/views/pages/election/comment_li.js
@@ -2,7 +2,7 @@ _.constructor('Views.Pages.Election.CommentLi', Monarch.View.Template, {
   content: function() { with(this.builder) {
     li({'class': "comment"}, function() {
       subview('avatar', Views.Components.Avatar, {imageSize: 34});
-      div({'class': "destroy"}).ref('destroyLink').click('destroy');
+      a({'class': "destroy"}, "×").ref('destroyLink').click('destroy');
       div({'class': "date"}).ref('createdAt');
       div({'class': "text"}, function() {
         div({'class': "name"}).ref('creatorName');
