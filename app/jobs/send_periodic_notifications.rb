1b903bf8c99d0d4c163b255562cc16c5e2ea5ecb
Nathan Sobo <nathansobo@gmail.com>
2011-07-05 15:35:11 -0700

    add logging to SendPeriodicNotifications

--- app/jobs/send_periodic_notifications.rb
+++ app/jobs/send_periodic_notifications.rb
@@ -3,14 +3,20 @@ module Jobs
     @queue = 'send_periodic_notifications'
 
     def perform
+      puts "sending #{period} notifications"
       users_to_notify = User.users_to_notify(period)
       total = users_to_notify.count
+      puts "about to email #{total} users"
       users_to_notify.each_with_index do |user, i|
         at(i + 1, total)
         next unless user.email_enabled?
+        puts "notifying #{user.email_address}"
         presenter = Views::NotificationMailer::NotificationPresenter.new(user, period)
         unless presenter.empty?
+          "actually sending non-empty notification"
           NotificationMailer.notification(user, presenter).deliver
+        else
+          puts "empty notification, skipping"
         end
       end
       completed
