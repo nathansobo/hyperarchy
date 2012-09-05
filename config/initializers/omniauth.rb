cf467447afccb5bb604ccb0c351e37def400d061
Nathan Sobo <nathan@github.com>
2012-09-05 14:13:25 -0600

    Remove :developer omniauth provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer # unless Rails.env.production?
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
 end
