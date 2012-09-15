69e56796b80b9628f67ff6ec4e356375fd789a8f
Nathan Sobo <nathan@github.com>
2012-09-14 22:56:49 -0600

    :lipstick:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    self.current_user = user = User.find_or_create_with_omniauth(auth)
+    self.current_user = User.find_or_create_with_omniauth(auth)
 
     redirect_to root_url
   end
