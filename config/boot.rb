a21549f78a0631c169f6a00b0dca0301b7d36aed
Nathan Sobo <nathan@github.com>
2012-09-19 14:32:03 -0600

    Don't require dotenv gem in production

--- config/boot.rb
+++ config/boot.rb
@@ -5,5 +5,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-require 'dotenv'
-Dotenv.load
+unless ENV['RAILS_ENV'] == 'production'
+  require 'dotenv'
+  Dotenv.load
+end
