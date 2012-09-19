af923dd3293f1a5973b6820c494add926c55c07b
Nathan Sobo <nathan@github.com>
2012-09-19 14:08:03 -0600

    Store Rails secret token in the .env file and update .env.example

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hyperarchy::Application.config.secret_token = 'b9406bb7a20b0479e8dd885464251b6c368cab60cbb7e959e927833a4f7359c52864603d040c285343a45af3c1bb7114f59e28b4020615b0d224b5478202a364'
+Hyperarchy::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
