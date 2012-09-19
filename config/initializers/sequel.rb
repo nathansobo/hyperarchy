ad816548c583791f8fd6f1763e5323cd632f53f2
Nathan Sobo <nathan@github.com>
2012-09-19 14:53:03 -0600

    Sequel initializer doesn't blow up if we're missing a configuration, just warns

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,6 +1,11 @@
 config = Rails.application.config.database_configuration[Rails.env]
-# ActiveRecord's adaptor is 'postgresql', but sequel's is Postgres
-# We're keeping ActiveRecord around in development for the migrations and rake tasks
-config['adapter'].gsub! 'postgresql', 'postgres'
-config['logger'] = Rails.logger
-Sequel.connect(config)
+
+unless config
+  $stderr.puts "Problem loading database configuration for #{Rails.env} environment"
+else
+  # ActiveRecord's adaptor is 'postgresql', but sequel's is Postgres
+  # We're keeping ActiveRecord around in development for the migrations and rake tasks
+  config['adapter'].gsub! 'postgresql', 'postgres'
+  config['logger'] = Rails.logger
+  Sequel.connect(config)
+end
