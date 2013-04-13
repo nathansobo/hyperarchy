309a5af68b8670bca0d664080215a4fe1b3014dd
Nathan Sobo <nathan@github.com>
2013-04-12 22:11:40 -0600

    Add user private channel auth to pusher controller

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -4,8 +4,21 @@ class PusherController < ApplicationController
   def auth
     channel_name = params[:channel_name]
     socket_id = params[:socket_id]
-    group_id = channel_name.match(/private-group-(\d+)/)[1].to_i
 
+    if match = channel_name.match(/private-group-(\d+)/)
+      group_id = match[1].to_i
+      auth_group_channel(channel_name, group_id, socket_id)
+    elsif match = channel_name.match(/private-user-(\d+)/)
+      user_id = match[1].to_i
+      auth_user_channel(channel_name, user_id, socket_id)
+    else
+      render :status => 404, :text => "Don't know how to auth to #{channel_name}"
+    end
+  end
+
+  private
+
+  def auth_group_channel(channel_name, group_id, socket_id)
     unless group = Group.find(group_id)
       render :text => "Group not found", :status => 404
       return
@@ -18,7 +31,13 @@ class PusherController < ApplicationController
     end
   end
 
-  private
+  def auth_user_channel(channel_name, user_id, socket_id)
+    if current_user_id == user_id
+      render :json => authenticate_on_channel(channel_name, socket_id)
+    else
+      render :text => "You're not that user", :status => 403
+    end
+  end
 
   def authenticate_on_channel(channel_name, socket_id)
     Pusher[channel_name].authenticate(socket_id)
--- spec/controllers/pusher_controller_spec.rb
+++ spec/controllers/pusher_controller_spec.rb
@@ -1,23 +1,39 @@
 require 'spec_helper'
 
-describe PusherController, :f do
+describe PusherController do
   describe "#auth" do
-    it "only allows members of a group to authenticate to its private channel", :f do
-      group = Group.make!
-      member = User.make!
-      non_member = User.make!
-      group.add_member(member)
+    describe "when authenticating to a group channel" do
+      it "only allows members of a group to authenticate to its private channel" do
+        group = Group.make!
+        member = User.make!
+        non_member = User.make!
+        group.add_member(member)
 
-      login_as(member)
-      post :auth, :channel_name => "private-group-#{group.id}", :socket_id => "123"
-      response.status.should == 200
+        login_as(member)
+        post :auth, :channel_name => "private-group-#{group.id}", :socket_id => "123"
+        response.status.should == 200
 
-      login_as(non_member)
-      post :auth, :channel_name => "private-group-#{group.id}", :socket_id => "123"
-      response.status.should == 403
+        login_as(non_member)
+        post :auth, :channel_name => "private-group-#{group.id}", :socket_id => "123"
+        response.status.should == 403
 
-      post :auth, :channel_name => "private-group-0", :socket_id => "123"
-      response.status.should == 404
+        post :auth, :channel_name => "private-group-0", :socket_id => "123"
+        response.status.should == 404
+      end
+    end
+
+    describe "when authenticating to a personal channel", :f do
+      it "only allows the user to authenticate to their own channel" do
+        user = User.make!
+        other_user = User.make!
+
+        login_as(user)
+        post :auth, :channel_name => "private-user-#{user.id}", :socket_id => "123"
+        response.status.should == 200
+
+        post :auth, :channel_name => "private-user-#{other_user.id}", :socket_id => "123"
+        response.status.should == 403
+      end
     end
   end
 end
