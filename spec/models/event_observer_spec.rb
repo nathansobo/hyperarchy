67e225240b9aa3bf3ddb22ab81e0e000308d1502
Nathan Sobo <nathan@github.com>
2013-04-13 21:30:37 -0600

    Fix EventObserver specs

--- spec/models/event_observer_spec.rb
+++ spec/models/event_observer_spec.rb
@@ -4,22 +4,24 @@ describe EventObserver do
   describe "#observe" do
     include BuildClientDataset
 
+    let(:group) { Group.make! }
     let(:events) { Hash.new {|h,k| h[k] = [] }}
     let(:current_user) { User.create! }
 
     before do
-      stub(Pusher[PUSHER_CHANNEL_PREFIX]).trigger_async do |channel, event|
+      stub(Pusher["private-#{PUSHER_CHANNEL_PREFIX}-group-#{group.id}"]).trigger_async do |channel, event|
         events[channel].push(event)
       end
 
       Prequel.session.current_user = current_user
       EventObserver.observe(Question, Answer)
+
     end
 
     it "causes all events on the given model classes to be sent to pusher" do
       freeze_time
 
-      question = Question.make!
+      question = group.questions.make!
       events['operation'].shift.should == ["create", "questions", question.wire_representation, build_client_dataset(current_user)]
 
       question.update :body => "Hello?"
@@ -30,7 +32,7 @@ describe EventObserver do
     end
 
     it "sends extra records for create events if desired" do
-      question = Question.make!
+      question = group.questions.make!
       events['operation'].clear
 
       answer = question.answers.make!
