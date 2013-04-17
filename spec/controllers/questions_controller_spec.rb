e889755e9031351f180e6a0823fbac3e6f6a1b01
Nathan Sobo <nathan@github.com>
2013-04-16 19:47:26 -0600

    Spec super user access to questions controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -14,46 +14,67 @@ describe QuestionsController do
       @non_member = User.make!
     end
 
-    it "allows private questions to be fetched if the user has permission" do
-      login_as(member)
+    describe "when the user is not superuser-enabled" do
+      it "allows private questions to be fetched if the user has permission" do
+        login_as(member)
 
-      # can't fetch until we have permission, which is only granted to non superusers when we fetch via the secret
-      get :show, :id => private_group_question.id.to_s
-      response.status.should == 404
+        # can't fetch until we have permission, which is only granted to non superusers when we fetch via the secret
+        get :show, :id => private_group_question.id.to_s
+        response.status.should == 404
 
-      get :show, :id => private_question.id.to_s
-      response.status.should == 404
+        get :show, :id => private_question.id.to_s
+        response.status.should == 404
 
-      # fetching via the secret works, and once we do so we can also fetch via id
-      get :show, :id => private_group_question.secret
-      response.status.should == 200
+        # fetching via the secret works, and once we do so we can also fetch via id
+        get :show, :id => private_group_question.secret
+        response.status.should == 200
 
-      get :show, :id => private_group_question.id.to_s
-      response.status.should == 200
+        get :show, :id => private_group_question.id.to_s
+        response.status.should == 200
 
-      get :show, :id => private_question.secret
-      response.status.should == 200
+        get :show, :id => private_question.secret
+        response.status.should == 200
 
-      get :show, :id => private_question.id.to_s
-      response.status.should == 200
+        get :show, :id => private_question.id.to_s
+        response.status.should == 200
+      end
+
+      it "only allows group questions to be fetched by group members" do
+        login_as(member)
+
+        get :show, :id => group_question.to_param
+        response.status.should == 200
+
+        get :show, :id => private_group_question.to_param
+        response.status.should == 200
+
+        login_as(non_member)
+
+        get :show, :id => group_question.to_param
+        response.status.should == 404
+
+        get :show, :id => private_group_question.to_param
+        response.status.should == 404
+      end
     end
 
-    it "only allows group questions to be fetched by group members" do
-      login_as(member)
+    describe "when the user is superuser enabled" do
+      before do
+        non_member.update!(:superuser_enabled => true)
+        login_as(non_member)
+      end
 
-      get :show, :id => group_question.to_param
-      response.status.should == 200
+      it "allows private and group questions to be fetched, even if the user is not a member of the group" do
+        get :show, :id => private_group_question.secret
+        response.status.should == 200
 
-      get :show, :id => private_group_question.to_param
-      response.status.should == 200
+        get :show, :id => group_question.to_param
+        response.status.should == 200
 
-      login_as(non_member)
+        get :show, :id => private_group_question.to_param
+        response.status.should == 200
+      end
 
-      get :show, :id => group_question.to_param
-      response.status.should == 404
-
-      get :show, :id => private_group_question.to_param
-      response.status.should == 404
     end
   end
 end
