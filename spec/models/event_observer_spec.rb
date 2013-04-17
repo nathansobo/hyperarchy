a3f266a0d5c088ff6ed5818b4d46d3fa5a2d21d4
Nathan Sobo <nathan@github.com>
2013-04-16 19:39:20 -0600

    Fix event observer spec for batched events

--- spec/models/event_observer_spec.rb
+++ spec/models/event_observer_spec.rb
@@ -9,34 +9,38 @@ describe EventObserver do
     let(:current_user) { User.create! }
 
     before do
-      stub(Pusher["private-group-#{group.id}"]).trigger_async do |channel, event|
-        events[channel].push(event)
-      end
-
       Prequel.session.current_user = current_user
       EventObserver.observe(Question, Answer)
-
     end
 
-    it "causes all events on the given model classes to be sent to pusher" do
+    def events_by_channel
+      Thread.current[:events_by_channel]
+    end
+
+    it "causes all events on the given model classes to be sent to pusher on the channel for the correct group / user" do
       freeze_time
 
       question = group.questions.make!
-      events['operation'].shift.should == ["create", "questions", question.wire_representation, build_client_dataset(current_user)]
+      event = events_by_channel["private-group-#{group.id}"].shift
+      event.should == ["create", "questions", question.wire_representation, build_client_dataset(current_user)]
 
       question.update :body => "Hello?"
-      events['operation'].shift.should == ["update", "questions", question.id, {"body"=>"Hello?"}]
+      event = events_by_channel["private-group-#{group.id}"].shift
+      event.should == ["update", "questions", question.id, {"body"=>"Hello?"}]
 
       question.destroy
-      events['operation'].shift.should == ["destroy", "questions", question.id]
+      event = events_by_channel["private-group-#{group.id}"].shift
+      event.should == ["destroy", "questions", question.id]
     end
 
     it "sends extra records for create events if desired" do
       question = group.questions.make!
-      events['operation'].clear
+      events_by_channel["private-group-#{group.id}"].clear
 
       answer = question.answers.make!
-      events['operation'].shift.should == [
+      event = events_by_channel["private-group-#{group.id}"].shift
+
+      event.should == [
         "create", "answers", answer.wire_representation, build_client_dataset(current_user)
       ]
     end
